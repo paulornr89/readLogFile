c43374e3149c4fa8296afa81a0de6a645421ae11
paulornr89 <paulornr89@gmail.com>
2023-10-18 12:45:40 -0300

    versão com exportação para planilha do excel está funcionando

--- readLogFile.js
+++ readLogFile.js
@@ -8,13 +8,13 @@
 
 const fs = require('fs');
 const { open } = require('node:fs/promises');
+const xl = require('excel4node');
 
 //console.log(fs.readFileSync("/server.log.2023-09-30"));
 
 try {
   fs.promises.readdir('./logs')
   .then(async files => {
-    console.log(files)
     const epiList = [];
 
     //LÊ O LOG
@@ -27,13 +27,15 @@ try {
     console.log(epiList);
     console.log("TOTAL DE EPIS: "  + epiList.length);
     //GRAVA JSON
-    fs.writeFile("logJson.json", JSON.stringify(epiList), err => {
+    // fs.writeFile("logJson.json", JSON.stringify(epiList), err => {
      
-        // Checking for errors
-        if (err) throw err; 
+    //     // Checking for errors
+    //     if (err) throw err; 
        
-        console.log("Done writing"); // Success
-    })
+    //     console.log("Done writing"); // Success
+    // })
+    console.log("GERANDO PLANILHA...");
+    makeSheet(epiList);
   })
 } catch (err) {
   console.error(err);
@@ -68,7 +70,7 @@ async function readLog (path , list) {
             }
             if(line.indexOf("dataEntrega") != -1){
                 contLocal += 1;
-                dataEntrega = getString(line);               
+                dataEntrega = line.substring(0,10).split("-").reverse().join("/");               
             }
             if(line.indexOf("horaEntrega") != -1){
                 contLocal += 1;
@@ -111,11 +113,11 @@ async function readLog (path , list) {
     console.log("RETIRADA POR DIA : " + cont)
   }
 
-  function getString(Line){
+function getString(Line){
     const string = Line.substring(Line.indexOf(">"));
 
     return string.substring(string.indexOf(":") + 1).trim();
-  }
+}
 
 function getColaborador(matricula){
     return fetch(`http://localhost:4000/colaborador/${matricula}/0101`)
@@ -129,4 +131,30 @@ function getColaborador(matricula){
     .then(response => response.json())
     .then(produto => produto[0].descricao)
     .catch(err => console.log(err))
+  }
+
+  function makeSheet(data) {
+    const wb = new xl.Workbook();
+    // Add Worksheets to the workbook
+    const ws = wb.addWorksheet('Lista de Retirada Imediata de EPI');
+    const columnNames = ["dataSolicitacao", "Código do EPI", "Descrição", "Matrícula", "Colaborador", "dataEntrega", "horaEntrega", "quantidadeEntrega"];
+
+    /**MONTA O CABEÇALHO */
+    let headingColumnIndex = 1; //diz que começará na primeira linha
+    columnNames.forEach(columnName => { //passa por todos itens do array
+        // cria uma célula do tipo string para cada título
+        ws.cell(1, headingColumnIndex++).string(columnName);
+    });
+
+    let rowIndex = 2;
+    data.forEach( record => {
+        let columnIndex = 1;
+        Object.keys(record).forEach(columnName =>{
+            ws.cell(rowIndex, columnIndex++)
+                .string(record [columnName])
+        });
+        rowIndex++;
+    }); 
+
+    wb.write('Lista de Retirada Imediata de EPI.xlsx');
   }
